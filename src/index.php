dee09715c9f401d0a56abde8b8fd1e1840c3f675
Šípka Michal <xsipkam1@node106.webte.fei.stuba.sk>
2024-05-08 23:07:44 +0200

    added bootstrap + responsive menu

--- src/index.php
+++ src/index.php
@@ -12,14 +12,19 @@ require_once("../../../configFinal.php");
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hlasovací systém</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php include "menu.php"; ?>
+    <?php include "menu.php"; ?>
 
-<div class="out-cont">
-    <form>
-        <?php
-        echo "<h2>skap</h2>";
-        ?>
-    </form>
-</div>
+    <div class="container border p-1 mt-4 shadow">
+        <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) : ?>
+            <h2>Prihlásený ako: <?= $_SESSION['login'] ?></h2>
+            <h2>Rola: <?= ($_SESSION['role'] === 'A' ? 'Admin' : 'User') ?></h2>
+        <?php else : ?>
+            <h2>Neprihlásený používateľ</h2>
+        <?php endif; ?>
+    </div>
+    
+</body>
--- src/menu.php
+++ src/menu.php
@@ -1,13 +1,41 @@
-<div id="menu">
-    <a href="index.php">Domov</a>
-    <?php
-    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-        echo '<a href="logout.php">Odhlásenie</a>';
-        echo '<span>Prihlásený ako ' . $_SESSION['login'] . '</span>';
-        echo '<span>Rola: ' . ($_SESSION['role'] === 'A' ? 'Admin' : 'User') . '</span>';
-    } else {
-        echo '<a href="login.php">Prihlásenie</a>';
-        echo '<a href="register.php">Registrácia</a>';
-    }
-    ?>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+    <body>
+    <div id="menu">
+        <header>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark p-0">
+                <div class="container-fluid">
+                    
+                    <a href="index.php">Domov</a>
+                    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-label="Expand">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="nav">
+                        <ul class="navbar-nav ms-auto">
+                            <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) : ?>
+                                <li class="nav-item">
+                                    <a href="logout.php" class="nav-link mx-3">Odhlásenie</a>
+                                </li>
+                            <?php else : ?>
+                                <li class="nav-item">
+                                    <a href="login.php" class="nav-link mx-3">Prihlásenie</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="register.php" class="nav-link mx-3">Registrácia</a>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
+    </div>
+</body>
+</html>
